5e0b3ed46019fb0a610d6df6c092972277088961
erro.126645 <erro.126645@e.unavarra.es>
2021-11-16 17:57:23 +0100

    definiciones

--- definiciones.h
+++ definiciones.h
@@ -7,12 +7,13 @@
 #define type_char 3
 #define type_string 4
 
-#define op_mayor
-#define op_menor
-#define op_distinto
-#define op_mayorigual
-#define op_menorigual
-
+#define op_mayor 5
+#define op_menor 6
+#define op_distinto 7
+#define op_mayorigual 8
+#define op_menorigual 9
+#define op_igual 10
+#define op_goto  11
 
 
 
