905aa5dcba68f23e8487cc2f62f469b107cb17d7
erro.126645 <erro.126645@e.unavarra.es>
2021-11-23 16:12:20 +0100

    tabla_de_cuadruplas

--- tabla_de_cuadruplas.h
+++ tabla_de_cuadruplas.h
@@ -5,10 +5,35 @@
 #include "definiciones.h"
 #include "tabla_de_simbolos.h"
 
+#define MAXCUADRUPLAS 1000
+
+typedef enum enumTipo {simbolo, booleano, entero, real} enumTipo;
+
+typedef struct dir_elemento{
+    enumTipo tipo;
+    union {
+
+        int constante_entera;
+        float contante_real;
+        bool constante_bool;
+        struct simbolo* simbolo;
+
+    }valor;
 
 
+}dir_elemento;
 
+typedef struct cuadrupla{
+    char *operador;
+    dir_elemento *operando1;
+    dir_elemento *operando2;
+    dir_elemento *resultado;
+}cuadrupla;
 
+typedef struct tablaCuadruplas{
+    cuadrupla *tabla[MAXCUADRUPLAS];
+    int nextquad;
+}tablaCuadruplas;
 
 
 
